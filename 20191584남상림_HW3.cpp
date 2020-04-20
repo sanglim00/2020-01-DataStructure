08158b635529aa4dc7bf24b890e9ffc686000c35
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-21 03:47:55 +0900

    Update 20191584남상림_HW3.cpp

--- 20191584남상림_HW3.cpp
+++ 20191584남상림_HW3.cpp
@@ -12,118 +12,97 @@ Algorithm :
 using namespace std;
 
 struct element {
-    int row;
-    int col;
-    int dir;
+    int row, col, dir;
 };
 struct offsets {
-    int vert;
-    int horiz;
+    int vert, horiz;
 };
+int maze[6][6] = { 0,1,1,1,1,1,  
+                            1,0,1,1,1,1,  
+                            1,0,0,0,0,1, 
+                            1,1,0,1,1,1,  
+                            1,0,1,0,0,1,  
+                            1,1,1,1,1,0 
+};
+int mark[6][6] = { 0,};
+
 int top = -1;
-int size = 10;
+int maxSize = 10;
+element stack[10];
 
-element stack[36];
-
+bool isEmpty(int top) {
+    if (top == -1) return true;
+    else return false;
+}
+bool isFull() {
+    if (top == maxSize - 1) return true;
+    else return false;
+}
 void push(int val) {
     if (!isFull())
         stack[++top] = val;
-    else
-        cout << "Stack is Full" << endl;
+    else cout << "Stack is Full" << endl;
 }
-int pop() {
-    if (!isEmpty())
-        return stack[top--];
-    else
-        cout << "Stack is Empty" << endl;
-}
-bool isEmpty() {
-    if (top == -1)
-        return true;
-    else
-        return false;
-}
-bool isFull() {
-    if (top == size - 1)
-        return true;
-    else
-        return false;
+void pop() {
+    if (!isEmpty(top))
+        cout << stack[top--] << endl;
+    else cout << "Stack is Empty" << endl;
 }
 
-int maze[6][6] = { 0,1,1,1,1,1, 
-                              1,0,1,1,1,1,  
-                              1,0,0,0,0,1,  
-                              1,1,0,1,1,1,  
-                              1,0,1,0,0,1,  
-                              1,1,1,1,1,0 
-};
-int mark[6][6] = {0,};
-
-void miroPath() {
-    int i, j;
-    int exit_row = 5, exit_col = 5;
+void miroPath(offsets move[]) {
     bool found = false;
     element position;
+
     mark[0][0] = 1;
-    int top = 0;
-    stack[0].row = 1;
-    stack[0].col = 1;
-    stack[0].dir = 1;
-    int next_row, next_col;
+    stack[0] = { 1, 1, 1 };
+
     int row, col, dir;
-  
-    while (!isEmpty() && !found) {
-        position = delete(&top);
+    int next_row, next_col;
+    int exit_row = 5, exit_col = 5;
+
+    while (!isEmpty && !found) {
+       // position = delete(&top);
         row = position.row;
         col = position.col;
         dir = position.dir;
         while (dir < 8 && !found) {
             next_row = row + move[dir].vert;
             next_col = col + move[dir].horiz;
-            if (next_row = exit_row && next_col == exit_col)
+
+            if (next_row == exit_row && next_col == exit_col)
                 found = true;
             else if (!maze[next_row][next_col] && !mark[next_row][next_col]) {
                 mark[next_row][next_col] = 1;
-                position.row = row;
-                position.col = col;
-                position.dir = ++dir;
+                position = { row, col, ++dir };
 
                 push(position.dir);
-                row = next_row;
-                col = next_col;
-                dir = 0;
+
+                row = next_row; col = next_col; dir = 0;
             }
             else
                 ++dir;
          }
-        for (i = 0; i < 6; i++) {
-            for (j = 0; j < 6; j++) {
-                if (maze[i][j] == 0)
-                    mark[i][j] = 1;
-            }
-        }
      }
     if (found) {
-        //미로 경로 출력
-        //방문된 matrix 출력
-        cout << "Marked maze : " << endl;
-        for (i = 0; i < 6; i++) {
-            for (j = 0; j < 6; j++) {
+        //miroPath 출력
+        cout << "The path is : " << endl;
+        //marked matrix 출력
+        cout << "Marked Matrix : " << endl;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 cout << mark[i][j];
             }
             cout << endl;
         }
     }
     else
-        cout << "The maze does not have a path" << endl;
+        cout << "The maze does not have a path\n" << endl;
+    
 }
-
-
 int main() {
-
     offsets move[8];
 
-    move[0].vert = -1; move[0].horiz = 0;   // N     
+    move[0].vert = -1; move[0].horiz = 0;   // N    
     move[1].vert = -1; move[1].horiz = 1;   // NE 
     move[2].vert = 0; move[2].horiz = 1;    // E     
     move[3].vert = 1; move[3].horiz = 1;    // SE   
@@ -132,5 +111,5 @@ int main() {
     move[6].vert = 0; move[6].horiz = -1;   // W     
     move[7].vert = -1; move[7].horiz = -1;  // NW 
 
-    miroPath();
+    miroPath(move);
 }
