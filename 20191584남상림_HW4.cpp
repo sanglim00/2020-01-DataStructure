f4b93be9a909f53140e7e887fa16b9d940a3ed44
Sanglim00 <54923245+Sanglim00@users.noreply.github.com>
2020-04-29 10:58:40 +0900

    Add files via upload

---
+++ 20191584남상림_HW4.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+private:
+	int data;
+	char name[10];
+	Node* next;
+	Node* prev;
+	Node(int val) {
+		data = val;
+		next = 0; prev = 0;
+	 }
+	friend class List;
+};
+
+class List {
+private:
+	Node* head;
+	Node* current;
+public:
+	List() { head = 0; current = 0; }
+	bool isEmpty();
+	void InsertAfter(int);
+	void InsertBefore(int);
+	void InsertFirst(int);
+	void InsertLast(int);
+	void DeleteCurrent();
+	int LocateCurrent(int);
+	void UpdateCurrent(int);
+	void DisplayCurrent();
+	void Display();
+};
+bool List::isEmpty() {
+	if (head == NULL) return true;
+	else return false;
+}
+
+void List::InsertAfter(int data) {
+	Node* temp = new Node(data);
+	Node* p, * q;
+
+	if (head == NULL) head = temp;
+	else if (current->next != NULL) {
+		
+	}
+	else {
+		p = head;
+		while (p) {
+			p = p->next;
+		}p = current;
+	}
+	current = temp;
+
+ }
+void List::InsertBefore(int data) {
+	Node* temp = new Node(data);
+
+	if (head == NULL) head = temp;
+	else if (temp->data == head->data) {
+		head = head->next;
+		head->prev = temp;
+	}
+	else {
+		temp->next = head;
+		head->prev = temp;
+		head = temp;
+	}
+	current = temp;
+}
+void List::InsertFirst(int data) {
+	Node* temp = new Node(data);
+	if (head == NULL) head = temp;
+	else {
+		head->prev = temp;
+		head = temp;
+	}
+	current = temp;
+}
+void List::InsertLast(int data) {
+	Node* temp = new Node(data);
+	Node* p;
+	if (head == NULL) head == temp;
+	else {
+		p = head;
+		while (p->next != 0) p = p->next;
+		p->next = temp;
+	}
+	current = temp;
+}
+void List::DeleteCurrent() {
+	Node* p, * q;
+
+	if (head == NULL) cout << "List is Empty.\n";
+	else if (current == head) {
+		p = head;
+		head = head->next;
+		head->prev = 0;
+		delete p;
+	}
+	else if(current->next == 0){
+	
+	}
+	else {
+		q = head; p = head;
+		while (p != 0 && p->data != current->data) {
+			q = p; p = p->next;
+		}if (p != 0) {
+			q->next = p->next;
+			if (p->next != 0) p->next->prev = q;
+			delete p;
+		}
+		else cout << current->data << "is not in the list\n";
+	}
+}
+int List::LocateCurrent(int data) {
+	Node* p;
+	p = head;
+	for (int i = 0; i < data; i++) p = p->next;
+	return p->data;
+}
+void List::UpdateCurrent(int data) {
+	current->data = data;
+}
+void List::DisplayCurrent() {
+	cout << current->data << endl;
+}
+void List::Display() {
+	Node* p;
+	p = head;
+	while (p) {
+		cout << p->data;
+		p = p->next;
+	}
+	cout << endl;
+}
+
+int main() {
+	List lst;
+	int choice, num;
+
+	while (true) {
+		cout << "1)InsertAfter 2)InsertBefore 3)InsertFirst 4)InsertLast 5)DeleteCurrent 6)LocateCurrent	 7)UpdateCurrent 8)Displaycurrent 9)DisplayList 10)Quit\n";
+		cout << "Choice : ";
+		cin >> choice;
+		switch (choice) {
+		case 1:
+			cout << "Enter a data to insert => ";
+			cin >> num;
+			lst.InsertAfter(num);
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 2:
+			cin >> num;
+			lst.InsertBefore(num);
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 3:
+			cin >> num;
+			lst.InsertFirst(num);
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 4:
+			cin >> num;
+			lst.InsertLast(num);
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 5:
+			lst.DeleteCurrent();
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 6:
+			cout << "Enter a position to locate => ";
+			cin >> num;
+			lst.LocateCurrent(num);
+			break;
+		case 7:
+			cout << "Enter a data to update => ";
+			cin >> num;
+			lst.UpdateCurrent(num);
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 8:
+			lst.DeleteCurrent();
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 9:
+			cout << "---------List---------\n";
+			lst.Display();
+			break;
+		case 10:
+			return 0;
+		}
+	 }
+	
+	
+}
